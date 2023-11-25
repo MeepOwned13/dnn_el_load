bdcc916d69d7df05553010ebe51f8bda5aa3a7f4
MeepOwned13 <sebokmatyas01@gmail.com>
2023-11-25 14:28:54 +0100

    Attention implemented
    Moved files to directories, implemented attention seq2seq model

--- models/torch_model_definitions.py
+++ models/torch_model_definitions.py
@@ -27,6 +27,9 @@ class GaussianNoise(nn.Module):
 # endregion
 
 
+# region Encoder-decoder
+
+
 class GRUEncoder(nn.Module):
     def __init__(self, features, embedding_size, num_layers=1, bidirectional=False, dropout=0.0, noise=0.0):
         super(GRUEncoder, self).__init__()
@@ -42,9 +45,9 @@ class GRUEncoder(nn.Module):
         x = self.noise(x)
         h_0 = (torch.zeros(self.num_layers * self.h_n_dim, batch_size, self.hidden_size)
                .requires_grad_().to(MODEL_DEFINITION_DEVICE))
-        _, hidden = self.gru(x, h_0)
+        out, hidden = self.gru(x, h_0)
 
-        return hidden
+        return out, hidden
 
 
 class GRUDecoder(nn.Module):
@@ -67,7 +70,7 @@ class GRUDecoder(nn.Module):
         out = self.dropout(out)
         out = self.fc(out)
 
-        return out, x, hidden
+        return out,hidden
 
 
 class Seq2seq(nn.Module):
@@ -85,16 +88,96 @@ class Seq2seq(nn.Module):
 
     def forward(self, x, y=None, teacher_forcing=0.0):
         batch_size = x.shape[0]
-        hidden = self.enc(x)
+        _, hidden = self.enc(x)
         dec_input = x[:, -1, 0].reshape(-1, 1, 1)  # this will be y_prev in my case
         output = torch.zeros(batch_size, self.pred_len).to(MODEL_DEFINITION_DEVICE)
 
         for i in range(self.pred_len):
-            out, _, _ = self.dec(dec_input, hidden)
+            out, hidden = self.dec(dec_input, hidden)
             output[:, i] = out[:, 0]
             if y is not None and torch.rand(1) < teacher_forcing:
-                dec_input = torch.cat((dec_input, y[:, i].reshape(-1, 1, 1)), dim=1)
+                dec_input = y[:, i].reshape(-1, 1, 1)
             else:
-                dec_input = torch.cat((dec_input, out.unsqueeze(1)), dim=1)
+                dec_input = out.unsqueeze(1)
 
         return output
+
+
+# endregion
+
+
+# region Attention encoder-decoder
+
+
+class BahdanauAttention(nn.Module):
+    """https://github.com/sooftware/attentions/blob/master/attentions.py"""
+    def __init__(self, hidden_dim: int) -> None:
+        super(BahdanauAttention, self).__init__()
+        self.query_proj = nn.Linear(hidden_dim, hidden_dim, bias=False)
+        self.key_proj = nn.Linear(hidden_dim, hidden_dim, bias=False)
+        self.bias = nn.Parameter(torch.rand(hidden_dim).uniform_(0.1))
+        self.score_proj = nn.Linear(hidden_dim, 1)
+
+    def forward(self, query, key, value):
+        score = self.score_proj(torch.tanh(self.key_proj(key) + self.query_proj(query) + self.bias)).squeeze(-1)
+        attn = nn.functional.softmax(score, dim=-1)
+        context = torch.bmm(attn.unsqueeze(1), value)
+        return context, attn
+
+
+class AttentionGRUDecoder(nn.Module):
+    def __init__(self, embedded_size, num_layers=1, bidirectional=False, dropout=0.0, noise=0.0):
+        super(AttentionGRUDecoder, self).__init__()
+        self.h_n_dim = 2 if bidirectional else 1
+        self.gru = nn.GRU(embedded_size * self.h_n_dim, embedded_size, num_layers,
+                          dropout=dropout, bidirectional=bidirectional, batch_first=True)
+        self.attention = BahdanauAttention(embedded_size * self.h_n_dim)
+        self.fc = nn.Sequential(
+            nn.Flatten(1, -1),
+            GaussianNoise(noise),
+            nn.Dropout(dropout),
+            nn.Linear(embedded_size * num_layers * self.h_n_dim, 1),
+        )
+
+    def forward(self, y_prev, hidden, enc_out):
+        query = hidden.permute(1, 0, 2).reshape(-1, 1, self.h_n_dim * self.gru.hidden_size)
+        attn_out, attn_weights = self.attention(query, enc_out, enc_out)
+
+        out, hidden = self.gru(attn_out, hidden)
+        out = self.fc(out + y_prev)
+
+        return out, hidden
+
+
+class AttentionSeq2seq(nn.Module):
+    def __init__(self, features=11, pred_len=3, embedding_size=10, num_layers=1, bidirectional=False,
+                 dropout=0.2, in_noise=0.0, out_noise=0.0, **kwargs):
+        super(AttentionSeq2seq, self).__init__()
+        self.pred_len = pred_len
+        self.features = features
+        self.embedding_size = embedding_size
+        self.num_layers = num_layers
+        self.enc = GRUEncoder(features, embedding_size, num_layers, bidirectional=bidirectional,
+                              dropout=dropout if num_layers > 1 else 0.0, noise=in_noise)
+        self.adec = AttentionGRUDecoder(embedding_size, num_layers, bidirectional=bidirectional,
+                                        dropout=dropout if num_layers > 1 else 0.0, noise=out_noise)
+
+    def forward(self, x, y=None, teacher_forcing=0.0):
+        batch_size = x.shape[0]
+        enc_out, hidden = self.enc(x)
+        dec_input = x[:, -1, 0].reshape(-1, 1, 1)  # this will be y_prev in my case
+        output = torch.zeros(batch_size, self.pred_len).to(MODEL_DEFINITION_DEVICE)
+
+        for i in range(self.pred_len):
+            out, hidden = self.adec(dec_input, hidden, enc_out)
+
+            output[:, i] = out[:, 0]
+
+            if y is not None and torch.rand(1) < teacher_forcing:
+                dec_input = y[:, i].reshape(-1, 1, 1)
+            else:
+                dec_input = out.unsqueeze(1)
+
+        return output
+
+# endregion
