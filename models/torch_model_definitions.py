4ed3499c4dd89a653c55c03588881914e2f0d1c8
MeepOwned13 <sebokmatyas01@gmail.com>
2023-11-28 16:38:34 +0100

    Fixing implementation
    Switch over to scaled dot product attention.
    Positional encoding now handled as separate input feature.

--- models/torch_model_definitions.py
+++ models/torch_model_definitions.py
@@ -1,10 +1,12 @@
 import torch
 import torch.nn as nn
+from math import log as math_log
+from math import sqrt as math_sqrt
+
 
 MODEL_DEFINITION_DEVICE = torch.device("cpu")
 if torch.cuda.is_available():
     MODEL_DEFINITION_DEVICE = torch.device("cuda")
-
 
 # region GaussianNoise, from: https://discuss.pytorch.org/t/writing-a-simple-gaussian-noise-layer-in-pytorch/4694/4
 
@@ -43,8 +45,7 @@ class GRUEncoder(nn.Module):
     def forward(self, x):
         batch_size = x.shape[0]
         x = self.noise(x)
-        h_0 = (torch.zeros(self.num_layers * self.h_n_dim, batch_size, self.hidden_size)
-               .requires_grad_().to(MODEL_DEFINITION_DEVICE))
+        h_0 = (torch.zeros(self.num_layers * self.h_n_dim, batch_size, self.hidden_size).to(MODEL_DEFINITION_DEVICE))
         out, hidden = self.gru(x, h_0)
 
         return out, hidden
@@ -111,25 +112,27 @@ class Seq2seq(nn.Module):
 
 class Attention(nn.Module):
     """from: https://github.com/sooftware/attentions/blob/master/attentions.py"""
-    def __init__(self, hidden_dim: int) -> None:
-        super(Attention, self).__init__()
-        self.query_proj = nn.Linear(hidden_dim, hidden_dim, bias=False)
-        self.key_proj = nn.Linear(hidden_dim, hidden_dim, bias=False)
-        self.bias = nn.Parameter(torch.rand(hidden_dim).uniform_(0.1))
-        self.score_proj = nn.Linear(hidden_dim, 1)
 
-    def forward(self, query, key, value):
-        score = self.score_proj(torch.tanh(self.key_proj(key) + self.query_proj(query) + self.bias)).squeeze(-1)
-        attn = nn.functional.softmax(score, dim=-1)
-        context = torch.bmm(attn.unsqueeze(1), value)
+    def __init__(self, dim: int):
+        super(Attention, self).__init__()
+        self.sqrt_dim = math_sqrt(dim)
+
+    def forward(self, query, key, value, mask=None):
+        score = torch.bmm(query, key.transpose(1, 2)) / self.sqrt_dim
+
+        if mask is not None:
+            score.masked_fill_(mask.view(score.size()), -float('Inf'))
+
+        attn = nn.functional.softmax(score, -1)
+        context = torch.bmm(attn, value)
         return context, attn
 
 
 class AttentionGRUDecoder(nn.Module):
-    def __init__(self, embedded_size, bidirectional=False, dropout=0.0, noise=0.0):
+    def __init__(self, input_size, embedded_size, bidirectional=False, dropout=0.0, noise=0.0):
         super(AttentionGRUDecoder, self).__init__()
         self.h_n_dim = 2 if bidirectional else 1
-        self.gru = nn.GRU(embedded_size * self.h_n_dim, embedded_size, 1,
+        self.gru = nn.GRU(embedded_size * self.h_n_dim + input_size, embedded_size, 1,
                           bidirectional=bidirectional, batch_first=True)
         self.attention = Attention(embedded_size * self.h_n_dim)
         self.fc = nn.Sequential(
@@ -143,8 +146,10 @@ class AttentionGRUDecoder(nn.Module):
         query = hidden.permute(1, 0, 2).reshape(-1, 1, self.h_n_dim * self.gru.hidden_size)
         attn_out, attn_weights = self.attention(query, enc_out, enc_out)
 
-        out, hidden = self.gru(attn_out, hidden)
-        out = self.fc(out + y_prev)
+        inp = torch.cat((y_prev, attn_out), dim=2)
+        out, hidden = self.gru(inp, hidden)
+
+        out = self.fc(out)
 
         return out, hidden
 
@@ -158,7 +163,7 @@ class AttentionSeq2seq(nn.Module):
         self.embedding_size = embedding_size
         self.enc = GRUEncoder(features, embedding_size, 1, bidirectional=bidirectional,
                               dropout=0.0, noise=in_noise)
-        self.adec = AttentionGRUDecoder(embedding_size, bidirectional=bidirectional,
+        self.adec = AttentionGRUDecoder(1, embedding_size, bidirectional=bidirectional,
                                         dropout=dropout, noise=out_noise)
 
     def forward(self, x, y=None, teacher_forcing=0.0):
@@ -180,12 +185,69 @@ class AttentionSeq2seq(nn.Module):
         return output
 
 
+# print(AttentionSeq2seq(bidirectional=True).to(MODEL_DEFINITION_DEVICE)(torch.zeros(1, 3, 11).to(MODEL_DEFINITION_DEVICE)))
 # endregion
 
 
 # region Positional encoding
 
 
-"""https://medium.com/@hunter-j-phillips/positional-encoding-7a93db4109e6"""
+class PositionalEncodingS2S(nn.Module):
+    def __init__(self, d_model, max_seq):
+        super(PositionalEncodingS2S, self).__init__()
 
+        pe = torch.zeros(max_seq, d_model)
+        position = torch.arange(0, max_seq, 1).unsqueeze(1).float()
+        div_term = torch.exp(torch.arange(0, d_model, 2).float()) * -(math_log(10_000.0) / d_model)
+
+        pe[:, 0::2] = torch.sin(position * div_term)
+        pe[:, 1::2] = torch.cos(position * div_term)
+
+        self.pe = pe.unsqueeze(0).to(MODEL_DEFINITION_DEVICE)
+
+    def forward(self, x):
+        expanded = self.pe[:, :x.shape[1]].expand((x.shape[0], -1, -1))
+        return torch.cat((x, expanded), dim=2)
+
+    def code_index(self, x, i):
+        expanded = self.pe[:, i].expand((x.shape[0], -1, -1))
+        return torch.cat((x, expanded), dim=2)
+
+
+class PosAttSeq2seq(nn.Module):
+    def __init__(self, features=11, pred_len=3, embedding_size=10, bidirectional=False,
+                 dropout=0.2, in_noise=0.0, out_noise=0.0, max_seq=72, **kwargs):
+        super(PosAttSeq2seq, self).__init__()
+        self.pred_len = pred_len
+        self.features = features
+        self.embedding_size = embedding_size
+        self.pe = PositionalEncodingS2S(1, max_seq)
+        self.enc = GRUEncoder(features, embedding_size - 1, 1, bidirectional=bidirectional,
+                              dropout=0.0, noise=in_noise)
+        self.adec = AttentionGRUDecoder(2, embedding_size, bidirectional=bidirectional,
+                                        dropout=dropout, noise=out_noise)
+
+    def forward(self, x, y=None, teacher_forcing=0.0):
+        batch_size = x.shape[0]
+        enc_out, hidden = self.enc(self.pe(x))
+        dec_input = x[:, -1, 0].reshape(-1, 1, 1)  # this will be y_prev in my case
+        output = torch.zeros(batch_size, self.pred_len).to(MODEL_DEFINITION_DEVICE)
+
+        for i in range(self.pred_len):
+            dec_input = self.pe.code_index(dec_input, i)  # pos encode decoder out
+            out, hidden = self.adec(dec_input, hidden, enc_out)
+
+            output[:, i] = out[:, 0]
+
+            if y is not None and torch.rand(1) < teacher_forcing:
+                dec_input = y[:, i].reshape(-1, 1, 1)
+            else:
+                dec_input = out.unsqueeze(1)
+
+        return output
+
+
+# print(PositionalEncodingS2S(1, 72).code_index((torch.zeros(2, 1, 1).to(MODEL_DEFINITION_DEVICE)), 2))
+# print(PosAttSeq2seq(bidirectional=True).to(MODEL_DEFINITION_DEVICE)(torch.zeros(1, 3, 11).to(MODEL_DEFINITION_DEVICE)))
 # endregion
+
